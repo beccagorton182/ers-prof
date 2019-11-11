7d3dccb7c8715bb0b5984fd91ff7aa41f073cd11
Rebecca Gorton <rebecca.gorton1@nhs.net>
2019-11-11 17:04:57 +0000

    appointments tidy up

--- app/assets/javascript/reveal-test.js
+++ app/assets/javascript/reveal-test.js
@@ -1,8 +1,26 @@
-function myFunction() {
-    var x = document.getElementById("service-details");
+  function myFunction() {
+    var x = document.getElementById("service-details-four");
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
       x.style.display = "none";
     }
   }
+  function myFunction() {
+    var x = document.getElementById("service-details-five");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  $('.service-details-link').click(function() {
+    var target = $(this).attr('data-target');
+ 
+    if($('#' + target).css('display') == "none")
+    {
+        $('#' + target).css('display', 'block');
+    } else {
+        $('#' + target).css('display', 'none');
+    }
+});
