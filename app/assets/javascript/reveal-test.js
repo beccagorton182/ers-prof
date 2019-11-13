e5d55ed4e283d6c4f0ee197e86f8571761dfa3db
Rebecca Gorton <rebecca.gorton1@nhs.net>
2019-11-13 13:08:02 +0000

    new summary screens

--- app/assets/javascript/reveal-test.js
+++ app/assets/javascript/reveal-test.js
@@ -1,19 +1,4 @@
-  function myFunction() {
-    var x = document.getElementById("service-details-four");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
-  function myFunction() {
-    var x = document.getElementById("service-details-five");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
+
   $('.service-details-link').click(function() {
     var target = $(this).attr('data-target');
  
